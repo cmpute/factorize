ac93952460fd3419812d7310a588f17c48fcd066
Jacob Zhong <cmpute@qq.com>
2022-04-26 10:42:53 -0400

    Add output format option

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,76 @@
-use clap::Parser;
+use clap::{ArgEnum, Parser};
 use num_bigint::BigUint;
-use num_prime::nt_funcs::{factorize64, factorize};
-use num_traits::ToPrimitive;
+use num_prime::nt_funcs::factorize;
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, ArgEnum)]
+enum OutputFormat {
+    MATH,
+    GNU,
+    JSON,
+}
 
 /// A command line utility for integer factorization
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
-    /// The integer to factorize
-    num: BigUint,
+    /// The integers to factorize
+    num: Vec<BigUint>,
+
+    /// Output format of the factorization result
+    #[clap(short, long, arg_enum, default_value_t=OutputFormat::MATH)]
+    format: OutputFormat,
 
     /// Print additional information on stderr
     #[clap(short, long)]
-    verbose: bool
+    verbose: bool,
 
-    // TODO: display input bits, ETA for big integers
+    /// Prove each factor to be prime using deterministic Miller-Rabin or Lucas's primality test
+    #[clap(short, long)]
+    prove: bool
+
+    // TODO: display input bits, ETA for big integers in verbose mode
+    // TODO: implement [Lucas test](https://en.wikipedia.org/wiki/Lucas_primality_test) and output it in json mode
 }
 
 fn main() {
     let args = Args::parse();
+    for n in args.num.into_iter() {
+        // print headers
+        match args.format {
+            OutputFormat::GNU => print!("{}:", n),
+            OutputFormat::MATH => print!("{} =", n),
+            OutputFormat::JSON => {}
+        };
 
-    // use shortcuts to prevent formatting big integers when possible
-    if let Some(n) = args.num.to_u64() {
-        println!("{:?}", factorize64(n));
-    } else {
-        println!("{:?}", factorize(args.num));
+        // factorize
+        let factors = factorize(n);
+
+        // print factors
+        match args.format {
+            OutputFormat::GNU => {
+                for (p, e) in factors.into_iter() {
+                    for _ in 0..e {
+                        print!(" {}", p);
+                    }
+                }
+                println!();
+            },
+            OutputFormat::JSON => println!("{{factors: {:?}, complete: true}}", factors),
+            OutputFormat::MATH => {
+                let mut first = true;
+                for (p, e) in factors.into_iter() {
+                    if !first {
+                        print!(" *");
+                    }
+                    if e == 1 {
+                        print!(" {}", p);
+                    } else {
+                        print!(" {}^{}", p, e);
+                    }
+                    first = false;
+                }
+                println!();
+            }
+        }
     }
 }
